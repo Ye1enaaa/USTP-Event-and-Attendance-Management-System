6a7f2837a4f2717adcb2d00c989b52a3165c4821
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-05-25 04:38:57 +0800

    put sidebar and topbar, ilang student gikan

--- resources/views/Users/student.blade.php
+++ resources/views/Users/student.blade.php
@@ -1,30 +1,3 @@
-<!-- {{-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="{{ asset('css/student.css') }}">
-
-    <title>Student</title>
-    
-</head>
-<body>
-    <div>
-        {{Auth::user()->email}}
-        {{Auth::user()->picture}}
-        @foreach($events as $event)
-            <h3>{{$event->eventName}}</h3>
-        @endforeach
-
-
-    </div>
-    
-</body>
-</html> --}}
-
-component -->
 
 @extends('Extras.side-navbar')
 
