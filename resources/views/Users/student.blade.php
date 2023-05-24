ece8794fb141410681f8faf49d99540defc886cf
joephinecalapiz <joephinecalapiz7@gmail.com>
2023-05-24 22:16:08 +0800

    reapply changes

--- resources/views/Users/student.blade.php
+++ resources/views/Users/student.blade.php
@@ -1,12 +1,14 @@
-{{-- <!DOCTYPE html>
+<!-- {{-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <link rel="stylesheet" href="{{ asset('css/student.css') }}">
 
     <title>Student</title>
+    
 </head>
 <body>
     <div>
@@ -22,9 +24,10 @@
 </body>
 </html> --}}
 
-<!-- component -->
+component -->
 
 @extends('Extras.side-navbar')
+
 
 @section('content')
 <head>
