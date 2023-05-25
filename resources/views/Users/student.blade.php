d50b94c125ec76437bc6c89156415d9afdfdcb29
m-montefalcon <mmontefalconschlprps@gmail.com>
2023-05-25 11:12:42 +0800

    fixed upcoming events, added a condition for past events

--- resources/views/Users/student.blade.php
+++ resources/views/Users/student.blade.php
@@ -9,19 +9,22 @@
 
     <div class="container">
         @foreach($events as $event)
-        <div class="child">
-            <a href="#{{$event->id}}" class="clickable-container">
-                <div class="event-container">
-                    <img src="http://127.0.0.1:8000/storage/{{$event->eventPicture}}" alt="" class="event-picture">
-                    <h3 class="event-name">{{$event->eventName}}</h3>
-                    <p class="event-details">Place: {{$event->eventPlace}}</p>
-                    <p class="event-details">Time: {{$event->eventTime}}</p>
-                    <p class="event-details">Date: {{$event->eventDate}}</p>
-                </div>
-            </a>
-        </div>
+            @if(strtotime($event->eventDate) > strtotime(date('Y-m-d')))
+            <div class="child">
+                <a href="#{{$event->id}}" class="clickable-container">
+                    <div class="event-container">
+                        <img src="http://127.0.0.1:8000/storage/{{$event->eventPicture}}" alt="" class="event-picture">
+                        <h3 class="event-name">{{$event->eventName}}</h3>
+                        <p class="event-details">Place: {{$event->eventPlace}}</p>
+                        <p class="event-details">Time: {{$event->eventTime}}</p>
+                        <p class="event-details">Date: {{$event->eventDate}}</p>
+                    </div>
+                </a>
+            </div>
+            @endif
         @endforeach
     </div>
+    
 
     <h1>Todays Events</h1>
     <div class="container">
