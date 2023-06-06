a4374fd23f12490fc21de4790fe70b85afa702f6
eunrose <117526702+eunrose@users.noreply.github.com>
2023-06-06 15:44:40 +0800

    update image

--- resources/views/Users/profile.blade.php
+++ resources/views/Users/profile.blade.php
@@ -1,50 +1,194 @@
-  
-  @extends('Extras.side-navbar')
+<!DOCTYPE html>
+<html lang="en">
 
-  
-    <div class="container rounded bg-white mt-5 mb-5">
-        <div class="row">
-            <div class="col-md-3 border-right">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Edit Account</title>
+  <style>
+    /* Add your custom CSS styles here */
+    .img-circle {
+      border-radius: 50%;
+    }
+    .img-size-2 {
+      width: 100px;
+      height: 100px;
+    }
+    .container {
+      max-width: 1200px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: white;
+    }
+    .row {
+      display: flex;
+      flex-wrap: wrap;
+      margin-right: -15px;
+      margin-left: -15px;
+    }
+    .col-md-6 {
+      flex: 0 0 50%;
+      max-width: 50%;
+      padding-right: 15px;
+      padding-left: 15px;
+    }
+    .panel {
+      margin-bottom: 20px;
+      background-color: #fff;
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      box-shadow: 0 1px 1px rgba(0,0,0,.05);
+    }
+    .panel-heading {
+      padding: 10px 15px;
+      border-bottom: 1px solid #ddd;
+      background-color: #f5f5f5;
+    }
+    .panel-heading span {
+      margin-right: 5px;
+    }
+    .panel-body {
+      padding: 15px;
+    }
+    .form-group {
+      margin-bottom: 15px;
+    }
+    .form-control {
+      display: block;
+      width: 100%;
+      height: 34px;
+      padding: 6px 12px;
+      font-size: 14px;
+      line-height: 1.42857143;
+      color: #555;
+      background-color: #fff;
+      background-image: none;
+      border: 1px solid #ccc;
+      border-radius: 4px;
+      box-shadow: inset 0 1px 1px rgba(0,0,0,.075);
+      transition: border-color ease-in-out .15s,box-shadow ease-in-out .15s;
+    }
+    .btn {
+      display: inline-block;
+      padding: 6px 12px;
+      margin-bottom: 0;
+      font-size: 14px;
+      font-weight: 400;
+      line-height: 1.42857143;
+      text-align: center;
+      white-space: nowrap;
+      vertical-align: middle;
+      -ms-touch-action: manipulation;
+      touch-action: manipulation;
+      cursor: pointer;
+      user-select: none;
+      background-image: none;
+      border: 1px solid transparent;
+      border-radius: 4px;
+      color: #fff;
+      background-color: #337ab7;
+      border-color: #2e6da4;
+    }
+    .btn-default {
+      color: #333;
+      background-color: #fff;
+      border-color: #ccc;
+    }
+    .btn-file {
+      position: relative;
+      overflow: hidden;
+    }
+    .btn-file input[type="file"] {
+      position: absolute;
+      top: 0;
+      right: 0;
+      min-width: 100%;
+      min-height: 100%;
+      font-size: 100px;
+      text-align: right;
+      opacity: 0;
+      outline: none;
+      background: white;
+      cursor: inherit;
+      display: block;
+    }
+  </style>
+</head>
 
-                <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"><span class="font-weight-bold">ADMIN</span><span class="text-black-50">sample@mail.com.my</span><span> </span>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <div class="panel-heading clearfix">
+                        <span class="glyphicon glyphicon-camera"></span>
+                        <span>Change My photo</span>
+                    </div>
                 </div>
-
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img class="img-circle img-size-2" src="" alt="" id="profileImage">
+                        </div>
+                        <div class="col-md-8">
+                            <form class="form" action="#" method="POST" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <input type="file" name="file_upload" multiple="multiple" class="btn btn-default btn-file" id="imageInput" />
+                                </div>
+                                <div class="form-group">
+                                    <input type="hidden" name="user_id" value="">
+                                    <button type="submit" name="submit" class="btn btn-warning">Change</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-
-            <div class="col-md-5 border-right">
-                <div class="p-3 py-5">
-                    <div class="d-flex justify-content-between align-items-center mb-3">
-                        <h4 class="text-right">Profile Settings</h4>
-                    </div>
-
-                    <div class="row mt-2">
-                        <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value=""></div>
-                        <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="" placeholder="surname"></div>
-                    </div>
-                    <div class="row mt-3">
-                        <div class="col-md-12"><label class="labels">Mobile Number</label><input type="text" class="form-control" placeholder="enter phone number" value=""></div>
-                    </div>
-                    <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+        </div>
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading clearfix">
+                    <span class="glyphicon glyphicon-edit"></span>
+                    <span>Edit My Account</span>
+                </div>
+                <div class="panel-body">
+                    <form method="post" action="#" class="clearfix">
+                        <div class="form-group">
+                            <label for="name" class="control-label">Name</label>
+                            <input type="name" class="form-control" name="name" value="">
+                        </div>
+                        <div class="form-group">
+                            <label for="username" class="control-label">Username</label>
+                            <input type="text" class="form-control" name="username" value="">
+                        </div>
+                        <div class="form-group clearfix">
+                            <button type="submit" name="update" class="btn btn-info">Update</button>
+                        </div>
+                    </form>
                 </div>
             </div>
             <div class="col-md-4">
-            </div>
         </div>
     </div>
-
-          <script>
-        document.getElementById("imageInput").addEventListener("change", function(event) {
-            var selectedFile = event.target.files[0];
-            
-            if (selectedFile) {
-                var reader = new FileReader();
-                reader.onload = function(event) {
-                    var profileImage = document.querySelector(".rounded-circle");
-                    profileImage.src = event.target.result;
-                };
-                reader.readAsDataURL(selectedFile);
-            }
-        });
-      </script>
+</div>
 
 
+
+  <script>
+    document.getElementById("imageInput").addEventListener("change", function(event) {
+      var selectedFile = event.target.files[0];
+
+      if (selectedFile) {
+        var reader = new FileReader();
+        reader.onload = function(event) {
+          var profileImage = document.querySelector("#profileImage");
+          profileImage.src = event.target.result;
+        };
+        reader.readAsDataURL(selectedFile);
+      }
+    });
+  </script>
+</body>
+
+</html>
