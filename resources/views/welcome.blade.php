592a15ed8835cf12d2bc16bd1340c009e71e07d5
Andres Tachado <andrestachado@gmail.com>
2023-06-07 13:40:25 +0800

    add logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,32 +14,33 @@
 <body class="bg-blue-100">
     <div class="bg-yellow-400 flex items-center">
         <div class="pl-4">
-            <img src="{{ asset('assets/pictures/ustp-logo.png') }}" class="h-14 w-14 object-contain">
+            <img src="{{ asset('assets/pictures/logo-logo.png') }}" class="h-14 w-14 object-contain">
         </div>
         <h1 class="pl-6 text-4xl font-bold font-serif text-blue-900">USTP-CDO EVENT</h1>
     </div>
 
-    <br>
+    <br><br><br>
 
-    <div class="max-w-md mx-auto mt-8 p-6 bg-white rounded-2xl shadow-2xl">
 
+    <div class="flex justify-end items-center">
+    <div class="flex justify-end items-center">
+        <img src="{{ asset('assets/pictures/logo-logo.png') }}" style="height: 500px; width: 800px; object-fit: contain;">  
+    </div>
+
+    <div class="max-w-md mx-auto mt-8 p-6 bg-white rounded-2xl shadow-2xl ml-92">
         <h1 class="flex items-center justify-center text-2xl font-serif text-black text-center">EVENT MANAGEMENT SYSTEM</h1> 
-
         <br>
-
         <div class="flex justify-center items-center">
-            <img src="{{ asset('assets/pictures/picture-event.png') }}" class="h-40 w-60 object-contain">  
+            <img src="{{ asset('assets/pictures/ustp-logo.png') }}" class="h-40 w-60 object-contain">  
         </div>
-
         <br><br> 
-
         <div class="flex justify-center"> 
             <form>
                 <div class="flex justify-center">
                     <button type="submit"> <a href="{{ route('user.login') }}"class="flex justify-center items-center bg-blue-900 hover:bg-blue-800 text-white font-bold py-2 px-10 font-serif rounded-lg">SIGN IN<a></button>
                 </div>
                 <br>
-                    <p class="text-center mt-0 p-4 text-gray-500">OR</p>
+                <p class="text-center mt-0 p-4 text-gray-500">OR</p>
                 <br>
                 <div class="flex justify-center">
                     <button type="submit"> <a href="{{ route('user.register') }}"class="flex justify-center items-center bg-blue-900 hover:bg-blue-800 text-white font-bold py-2 px-10 font-serif rounded-lg">SIGN UP<a></button>
@@ -47,7 +48,8 @@
                 <br><br>    
             </form>
         </div>
-    </div>
+    </div>  
+</div>
     <br><br>
 </body>
 </html>
