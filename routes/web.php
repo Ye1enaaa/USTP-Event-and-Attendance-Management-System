6b873bac6c360558433b9d9b1835ac7fe988f184
EErickson <avilasonson@gmail.com>
2023-06-29 09:12:19 +0800

    added middleware on web route

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,14 @@ Route::controller(LoginController::class)->group(function(){
 
 
 //Views for student or Admin Dashboard
-Route::get('/admin' , [ViewsController::class, 'returnAdminDashboardView']);
-Route::get('/student' , [ViewsController::class, 'returnStudentDashboardView']);
-Route::get('/studentprofile', [ViewsController::class, 'viewStudentProfile'])->name('studentprofile');
+Route::get('/admin' , [ViewsController::class, 'returnAdminDashboardView'])->middleware('admin');
+Route::get('/student' , [ViewsController::class, 'returnStudentDashboardView'])->middleware('student');
+Route::get('/studentprofile', [ViewsController::class, 'viewStudentProfile'])->name('studentprofile')->middleware('student');
 
 
 
 //EventController
-Route::controller(EventController::class)->group(function(){
+Route::controller(EventController::class)->middleware('admin')->group(function(){
     Route::get('/createevent','returnCreateEventView');
     Route::post('/addevent', 'addEvent')->name('add.event');
     // Route::delete('/delete/{id}', 'destroy')->name('delete.event');
@@ -51,7 +51,7 @@ Route::controller(EventController::class)->group(function(){
 
 
 //EventDetailsController
-Route::get('users/event/details/{id}/attended/{studentId}', [EventDetailsController::class, 'show'])->name('event.details');
-Route::get('admin/event/details/{id}', [EventDetailsController::class, 'showEventDetailsAdmin']);
+Route::get('users/event/details/{id}/attended/{studentId}', [EventDetailsController::class, 'show'])->name('event.details')->middleware('student');
+Route::get('admin/event/details/{id}', [EventDetailsController::class, 'showEventDetailsAdmin'])->middleware('admin');
 Route::get('/profile', [ProfileController::class, 'showProfile']);
 Route::patch('/edit-profile/{id}',[ProfileController::class, 'editProfile']);
